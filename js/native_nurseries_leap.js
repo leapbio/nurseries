683a70a67b07741e4c587400177b3dd458fed82c
indiajohnson-cm <46683218+indiajohnson-cm@users.noreply.github.com>
2019-12-23 10:10:17 -0500

    Update Cardno Website info

--- js/native_nurseries_leap.js
+++ js/native_nurseries_leap.js
@@ -32,7 +32,7 @@ var geoJson = ({
         "Owner_Con": "mull",
         "Sales_Con": "mull",
         "General E": "mull",
-        "Facebook": "www.cardnojfnew.com",
+        "Facebook": "http://www.cardnonativeplantnursery.com/",
         "marker-color": "#254503",
         "marker-size": "small",
         "marker-symbol": "garden"
