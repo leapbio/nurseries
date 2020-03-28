4a4856d519b25cef43244b4e6ed8959e1ee9ef82
indiajohnson-cm <46683218+indiajohnson-cm@users.noreply.github.com>
2020-03-28 17:37:09 -0400

    Removed "nan, nan" entries

--- js/native_nurseries_leap.js
+++ js/native_nurseries_leap.js
@@ -1,4 +1,3 @@
-
 L.mapbox.accessToken = 'pk.eyJ1IjoiY21pbnZhc2l2ZXBsYW50cyIsImEiOiJmdGpSeF9jIn0.G_WIbBv2lsun6xHHXgIH8g';
 
 var southWest = L.latLng(33.064,
@@ -124,40 +123,6 @@ var geoJson = ({
     {
         'type': 'Feature',
         'properties': {
-            'Timestamp': '1/23/2020 9: 53: 47',
-            'Nursery_Status': 'New Nursery',
-            'Nursery': 'Agrecol Native Nursery',
-            'State_1': 'WI',
-            'Type': 'Wholesale, Retail, Online, Open to the Public, Catalog',
-            'Native _%': '99',
-            'Native': 'Yes',
-            'Keep': 'Seed, Plants, Herbaceous',
-            'Grow_Type': 'Grown from known seed locations (can track provenance), Germinated from seed, Grown by clonal dividing, Grown from starter plugs',
-            'Specialty': 'Organic, Wetland Restoration, Native Plants, Bioengineering Material',
-            'Address': '10101 N Casey Rd, Evansville',
-            'Owner_Con': 'Nursery Sales',
-            'Contact_Number': '(608) 223-3571',
-            'Contact_Email': 'ecosolutions@agrecol.com',
-            'Preferred_Contact': 'Phone, Email',
-            'Notes': 'Free Shipping for Online Plant Orders! We ship May through September. Visit our website for details.',
-            'Questions and comments': nan,
-            'Zip': 53536.0,
-            'Facebook': nan,
-            'Test': nan,
-            'location': None,
-            'point': None,
-            'Latitude': nan,
-            'Longitude': nan,
-            'elevation': nan
-        },
-        'geometry': {
-            'type': 'Point',
-            'coordinates': [nan, nan]
-        }
-    },
-    {
-        'type': 'Feature',
-        'properties': {
             'Timestamp': '1/23/2020 10: 55: 25',
             'Nursery_Status': 'New Nursery',
             'Nursery': 'Dropseed Native Plant Nursery',
@@ -193,40 +158,7 @@ var geoJson = ({
             ]
         }
     },
-    {
-        'type': 'Feature',
-        'properties': {
-            'Timestamp': '1/23/2020 13: 38: 43',
-            'Nursery_Status': 'New Nursery',
-            'Nursery': 'Wildtype Native Plants & Ecological Services',
-            'State_1': 'MI',
-            'Type': 'Wholesale, Open to the Public, Catalog',
-            'Native _%': '99',
-            'Native': 'Yes',
-            'Keep': 'Plants, Herbaceous, Ferns, Woody, Shrubs, Containerized Trees',
-            'Grow_Type': 'Grown from known seed locations (can track provenance), Germinated from seed',
-            'Specialty': 'Wetland Restoration, Native Plants, Bioengineering Material',
-            'Address': '900 N. Every Road Mason ',
-            'Owner_Con': 'Bill Schneider',
-            'Contact_Number': '517.244.1140',
-            'Contact_Email': 'info@wildtypeplants.com',
-            'Preferred_Contact': 'Phone, Email',
-            'Notes': 'we are primarily a wholesale nursery but we are open to the public 12 days a season - see our website for more detail wildtypeplants.com',
-            'Questions and comments': 'Thanks!!!!',
-            'Zip': 48854.0,
-            'Facebook': nan,
-            'Test': nan,
-            'location': None,
-            'point': None,
-            'Latitude': nan,
-            'Longitude': nan,
-            'elevation': nan
-        },
-        'geometry': {
-            'type': 'Point',
-            'coordinates': [nan, nan]
-        }
-    },
+    
     {
         'type': 'Feature',
         'properties': {
@@ -265,40 +197,7 @@ var geoJson = ({
             ]
         }
     },
-    {
-        'type': 'Feature',
-        'properties': {
-            'Timestamp': '1/23/2020 15: 18: 34',
-            'Nursery_Status': 'New Nursery',
-            'Nursery': 'Alpha Nurseries Inc.',
-            'State_1': 'MI',
-            'Type': 'Wholesale',
-            'Native _%': '85',
-            'Native': 'Yes',
-            'Keep': 'Plants, Woody, Shrubs, Bare Root',
-            'Grow_Type': 'Grown from known seed locations (can track provenance), Germinated from seed, Grown by clonal dividing, Grown from starter plugs',
-            'Specialty': 'Wetland Restoration, Native Plants',
-            'Address': '3737 65th St, Holland',
-            'Owner_Con': 'Jeff Busscher',
-            'Contact_Number': '269-857-7804',
-            'Contact_Email': 'info@alphanurseries.com',
-            'Preferred_Contact': 'Email',
-            'Notes': 'We are a bare root tree nursery featuring 250 species of mostly native plants',
-            'Questions and comments': 'Thank-you for allowing us to join your site!',
-            'Zip': 49423.0,
-            'Facebook': nan,
-            'Test': nan,
-            'location': None,
-            'point': None,
-            'Latitude': nan,
-            'Longitude': nan,
-            'elevation': nan
-        },
-        'geometry': {
-            'type': 'Point',
-            'coordinates': [nan, nan]
-        }
-    },
+    
     {
         'type': 'Feature',
         'properties': {
@@ -490,40 +389,7 @@ var geoJson = ({
             ]
         }
     },
-    {
-        'type': 'Feature',
-        'properties': {
-            'Timestamp': '1/26/2020 9: 22: 13',
-            'Nursery_Status': 'New Nursery',
-            'Nursery': 'Native Ohio Plants, LLC',
-            'State_1': 'OH',
-            'Type': 'Retail, Appointment Only, Small/Home Business',
-            'Native _%': '100',
-            'Native': 'Yes',
-            'Keep': 'Plants, Woody, Shrubs, Containerized Trees',
-            'Grow_Type': 'Grown from known seed locations (can track provenance), Germinated from seed',
-            'Specialty': 'Native Plants',
-            'Address': '2680 West Charleston Rd, Tipp City, OH',
-            'Owner_Con': 'Ron Corbett',
-            'Contact_Number': '(937) 524-4058',
-            'Contact_Email': 'nativeohio@earthlink.net',
-            'Preferred_Contact': 'Email',
-            'Notes': 'Visits to the nursery can be made by appointment or at sales shown on the nursery website (nativeohioplants.com).  The nursery specializes in container grown trees, shrubs, and wildflowers native to Ohio that have been propagated from locally gathered seed.',
-            'Questions and comments': nan,
-            'Zip': 45371.0,
-            'Facebook': nan,
-            'Test': nan,
-            'location': None,
-            'point': None,
-            'Latitude': nan,
-            'Longitude': nan,
-            'elevation': nan
-        },
-        'geometry': {
-            'type': 'Point',
-            'coordinates': [nan, nan]
-        }
-    },
+ 
     {
         'type': 'Feature',
         'properties': {
